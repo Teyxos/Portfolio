4effd44ae4f75082bdfb091fdd43eb34053707ca
Teyxos <antonio77youtu@gmail.com>
2022-02-14 09:55:22 +0100

    Revert "Carousel started"
    This reverts commit 8cf13d746b81f7ebaa7c6d7bafd65f0f2185126c.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,17 +1,13 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-function Carousel({ children }) {
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  overflow: hidden;
+`;
 
-  const Wrapper = styled.div`
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: flex-start;
-    overflow: hidden;
-    margin-left: -0px;
-  `;
+function Carousel({ children }) {
   return <Wrapper>{children}</Wrapper>;
 }
 
